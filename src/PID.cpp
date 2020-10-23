2a225a83579a53168f503ca82532f8ed46e7a1c6
Xingshu Sun <xingshusun@Xingshus-MacBook-Pro-2.local>
2020-10-22 23:28:13 -0700

    project finished

--- src/PID.cpp
+++ src/PID.cpp
@@ -9,9 +9,11 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp_, double Ki_, double Kd_) {
-  /**
-   * TODO: Initialize PID coefficients (and errors, if needed)
-   */
+  
+  Kp = Kp_;
+  Ki = Ki_;
+  Kd = Kd_;
+
 
 }
 
@@ -19,12 +21,18 @@ void PID::UpdateError(double cte) {
   /**
    * TODO: Update PID errors based on cte.
    */
-
+  d_error = cte - p_error;
+  p_error = cte;
+  i_error += cte;
 }
 
 double PID::TotalError() {
-  /**
-   * TODO: Calculate and return the total error
-   */
-  return 0.0;  // TODO: Add your total error calc here!
+  double steer = -Kp * p_error - Kd * d_error - Ki * i_error;
+  if (steer < -1) {
+    steer = -1;
+  }
+  if (steer > 1) {
+    steer = 1;
+  }
+  return steer;
 }
